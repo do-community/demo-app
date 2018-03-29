ca2a191eb843187b2576ca71080deba6237b5806
trotttrotttrott <trott@odaacabeef.com>
2018-03-29 13:12:44 -0700

    Eliminate spaces usage
    This is intended to be temporary. There are currently issues with the
    spaces API that prevents creation and deletion. It is intended to
    restore spaces functionality when issues are resolved.

--- statuspage-launch.sh
+++ statuspage-launch.sh
@@ -21,62 +21,10 @@ cat >> /root/.bashrc << EOF
 export TF_VAR_token=$do_token
 EOF
 export bastion_id=$(curl -s http://169.254.169.254/metadata/v1/id)
-cat > bucket_create.py << EOF
-import boto3
-from botocore.client import Config
-session = boto3.session.Session()
-client = session.client('s3',
-                        endpoint_url='https://nyc3.digitaloceanspaces.com',
-                        aws_access_key_id='$do_spaces_id',
-                        aws_secret_access_key='$do_spaces_key')
-response = client.list_buckets()
-spaces = [space['Name'] for space in response['Buckets']]
-if 'tf-states-$bastion_id' in spaces:
-  print("Spaces List: %s" % spaces)
-else:
-  client.create_bucket(Bucket='tf-states-$bastion_id')
-EOF
-cat > bucket_delete.py << EOF
-import boto3
-s3 = boto3.resource('s3',
-                        endpoint_url='https://nyc3.digitaloceanspaces.com',
-                        aws_access_key_id='$do_spaces_id',
-                        aws_secret_access_key='$do_spaces_key')
-session = boto3.session.Session()
-client = session.client('s3',
-                        endpoint_url='https://nyc3.digitaloceanspaces.com',
-                        aws_access_key_id='$do_spaces_id',
-                        aws_secret_access_key='$do_spaces_key')
-response = client.list_buckets()
-spaces = [space['Name'] for space in response['Buckets']]
-if 'tf-states-$bastion_id' in spaces:
-  tf_bucket = s3.Bucket('tf-states-$bastion_id')
-  for key in tf_bucket.objects.all():
-    key.delete()
-  tf_bucket.delete()
-EOF
-cat > terraform/remote.tf << EOF
-terraform {
-  backend "s3" {
-    bucket = "tf-states-$bastion_id"
-    key    = "status-page-demo/terraform.tfstate"
-    region = "us-east-1"
-    endpoint = "https://nyc3.digitaloceanspaces.com"
-    access_key = "$do_spaces_id"
-    secret_key = "$do_spaces_key"
-    skip_credentials_validation = true
-    skip_get_ec2_platforms = true
-    skip_requesting_account_id = true
-    skip_metadata_api_check = true
-  }
-}
-EOF
-python bucket_create.py && rm -f bucket_create.py
 cat > cleanup.sh << EOF
 cd /root/statuspage-demo/terraform
 terraform state rm digitalocean_droplet.bastion
 terraform destroy -force
-python /root/statuspage-demo/bucket_delete.py
 EOF
 chmod +x cleanup.sh
 cd terraform
