2796a774bb0d35686a0e93b856362e6084cdd76d
trotttrotttrott <trott@odaacabeef.com>
2018-03-29 18:32:05 -0700

    Refactor statuspage-launch.sh
    Launch script leverages localhost Ansible playbook for installing
    necessary components.

--- statuspage-launch.sh
+++ statuspage-launch.sh
@@ -1,33 +1,25 @@
-#!/bin/sh
+#!/bin/bash -x
 
-export TF_VAR_token=$do_token
+set -e
 
+# Install Ansible
 apt-add-repository ppa:ansible/ansible
 apt update
-apt install -y unzip software-properties-common python-boto3 apache2-utils ansible
-apt autoremove -y
-ssh-keygen -P "" -f /root/.ssh/id_rsa
-wget https://releases.hashicorp.com/terraform/0.11.3/terraform_0.11.3_linux_amd64.zip -O terraform_0.11.3_linux_amd64.zip
-unzip terraform_0.11.3_linux_amd64.zip
-mv terraform /usr/bin/terraform
-rm terraform_0.11.3_linux_amd64.zip
-cd ~
-#TODO: Update this URL to GH release URL when this repository becomes public
-wget https://statuspage-demo.nyc3.digitaloceanspaces.com/statuspage-demo.zip
-unzip statuspage-demo.zip -d statuspage-demo
-rm -f statuspage-demo.zip
-cd statuspage-demo
-cat >> /root/.bashrc << EOF
-export TF_VAR_token=$do_token
-EOF
-export bastion_id=$(curl -s http://169.254.169.254/metadata/v1/id)
-cat > cleanup.sh << EOF
+apt install -y ansible
+
+# Download the demo app
+wget -P /root https://statuspage-demo.nyc3.digitaloceanspaces.com/statuspage-demo.tar.gz
+mkdir /root/statuspage-demo
+tar -xf /root/statuspage-demo.tar.gz -C /root/statuspage-demo
+rm /root/statuspage-demo.tar.gz
+
+# Run the localhost playbook
+cd /root/statuspage-demo/ansible
+HOME=/root ansible-playbook playbooks/localhost.yml --connection=local --inventory=localhost,
+
+# Run Terraform.
 cd /root/statuspage-demo/terraform
-terraform state rm digitalocean_droplet.bastion
-terraform destroy -force
-EOF
-chmod +x cleanup.sh
-cd terraform
-terraform init -backend
-terraform import digitalocean_droplet.bastion $bastion_id
+echo "token = \"$do_token\"" > token.auto.tfvars
+terraform init
+terraform import digitalocean_droplet.bastion $(curl -s http://169.254.169.254/metadata/v1/id)
 terraform apply -auto-approve
